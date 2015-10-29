561306089c243b4d9010769551f03230933192a5
Jake Hatfield <jhatfield013@hotmail.com>
2015-10-28 22:21:40 -0400

    Added JSON methods

--- GameSpace/src/main/java/com/mkyong/rest/HelloWorldService.java
+++ GameSpace/src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -1,29 +1,127 @@
 package com.mkyong.rest;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+
 
 @Path("/hello")
 public class HelloWorldService
 {
 
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg)
+	@Path("/events")
+	public Response getEvent()
 	{
 		TomCatManager dataBase = new TomCatManager();
-		ArrayList<CalendarEvent> events;
+		JSONArray events;
 		events = dataBase.getEvents("select * from calendarEvent");
 		String output = "";
 
-		for(CalendarEvent ce: events)
-		output += ce;
+		for(Object json: events)
+		output += json;
  
 		return Response.status(200).entity(output).build();
  
 	}
- 
+
+	@GET
+	@Path("/accounts")
+	public Response getAccount()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray account;
+		account = dataBase.getAccount("select * from account");
+		String output = "";
+
+		for(Object json: account)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/events/attending")
+	public Response getAttending()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray attending;
+		attending = dataBase.getAttending("select * from attending");
+		String output = "";
+
+		for(Object json: attending)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/contacts")
+	public Response getContact()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray contact;
+		contact = dataBase.getContact("select * from contact");
+		String output = "";
+
+		for(Object json: contact)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/friends")
+	public Response getFriend()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray friend;
+		friend = dataBase.getFriend("select * from friend");
+		String output = "";
+
+		for(Object json: friend)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/threads/submitted")
+	public Response getPost()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray post;
+		post = dataBase.getPost("select * from post");
+		String output = "";
+
+		for(Object json: post)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/threads")
+	public Response getThread()
+	{
+		TomCatManager dataBase = new TomCatManager();
+		JSONArray thread;
+		thread = dataBase.getThread("select * from thread");
+		String output = "";
+
+		for(Object json: thread)
+			output += json;
+
+		return Response.status(200).entity(output).build();
+
+	}
 }
--- GameSpace/src/main/java/com/mkyong/rest/TomCatManager.java
+++ GameSpace/src/main/java/com/mkyong/rest/TomCatManager.java
@@ -1,5 +1,8 @@
 package com.mkyong.rest;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -68,24 +71,178 @@ public class TomCatManager
         }
     }
 
-    public ArrayList<CalendarEvent> getEvents(String query)
+    public JSONArray getEvents(String query)
     {
-        ArrayList output = new ArrayList<CalendarEvent>();
+        JSONArray output = new JSONArray();
         try
         {
             results = statement.executeQuery(query);
 
                     while(results.next() )
                     {
-                        CalendarEvent event = new CalendarEvent();
-                        event.name = results.getString("eventName");
-                        event.dateTime = results.getString("eventDateTime");
-                        event.game = results.getString("eventGame");
-                        event.location = results.getString("eventLocation");
-                        event.description = results.getString("eventDescription");
+                        JSONObject event = new JSONObject();
+                        event.put("name", results.getString("eventName") );
+                        event.put("dateTime", results.getString("eventDateTime"));
+                        event.put("game", results.getString("eventGame"));
+                        event.put("location", results.getString("eventLocation") );
+                        event.put("description", results.getString("eventDescription") );
 
                         output.add(event);
                     }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getAccount(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject account = new JSONObject();
+                account.put("Username", results.getString("accountUsername") );
+                account.put("Password", results.getString("accountPassword") );
+                account.put("Email", results.getString("accountEmail") );
+                account.put("FirstName", results.getString("accountFirstName") );
+                account.put("LastName", results.getString("accountLastName") );
+                account.put("Birthday", results.getString("accountBirthday") );
+                account.put("Privacy", results.getString("accountPrivacy") );
+
+
+                output.add(account);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getAttending(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject attending = new JSONObject();
+                attending.put("Event", results.getString("FK_eventID") );
+                attending.put("Account", results.getString("FK_accountID") );
+
+                output.add(attending);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getContact(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject contact = new JSONObject();
+                contact.put("AccountID", results.getString("FK_accountID") );
+                contact.put("CharacterName", results.getString("contactCharacter_AccountName") );
+                contact.put("GameService", results.getString("contactGame_ServiceName") );
+                contact.put("ServerName", results.getString("contactServerName") );
+
+                output.add(contact);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getFriend(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject friend = new JSONObject();
+                friend.put("FriendID1", results.getString("FK_accountID1") );
+                friend.put("FriendID2", results.getString("FK_accountID2") );
+                friend.put("ApprovedFriend", results.getString("friendIsApproved") );
+
+                output.add(friend);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getPost(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject post = new JSONObject();
+                post.put("Thread", results.getString("FK_threadID") );
+                post.put("UserAccount", results.getString("FK_accountID") );
+                post.put("Submitted", results.getString("postContent") );
+
+                output.add(post);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    public JSONArray getThread(String query)
+    {
+        JSONArray output = new JSONArray();
+        try
+        {
+            results = statement.executeQuery(query);
+
+            while(results.next() )
+            {
+                JSONObject thread = new JSONObject();
+                thread.put("ThreadName", results.getString("threadName") );
+                thread.put("ThreadGame", results.getString("threadGame") );
+
+                output.add(thread);
+            }
         }
         catch (Exception e)
         {
